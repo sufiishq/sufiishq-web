fabf20f016530b84fe9f730749d6f6c675e13aff
Syed Owais Ali <owais.ali@venturedive.com>
2021-10-28 23:11:00 +0500

    enable route in navigations

--- src/app/core/components/nav/nav.component.ts
+++ src/app/core/components/nav/nav.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ThemeService} from "../../services/theme.service";
 import {MatDialog} from "@angular/material/dialog";
 import {SettingsComponent} from "../settings/settings/settings.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'si-nav',
@@ -12,13 +13,13 @@ export class NavComponent implements OnInit {
 
   navigation: any[]
 
-  constructor(public themeService: ThemeService, public dialog: MatDialog) {
+  constructor(public themeService: ThemeService, public dialog: MatDialog, private router: Router) {
     this.navigation = [
-      {id: 1, icon: 'kalam', text: 'Kalam'},
-      {id: 2, icon: 'drum', text: 'Urs'},
-      {id: 3, icon: 'dish', text: 'Niaz'},
-      {id: 4, icon: 'pin', text: 'Locaion'},
-      {id: 5, icon: 'contact', text: 'Contact'},
+      {id: 1, icon: 'kalam', text: 'Kalam', route: ''},
+      {id: 2, icon: 'drum', text: 'Urs', route: 'urs'},
+      {id: 3, icon: 'dish', text: 'Niaz', route: 'niaz'},
+      {id: 4, icon: 'pin', text: 'Location', route: 'location'},
+      {id: 5, icon: 'contact', text: 'Contact', route: 'contact'},
       {id: 6, icon: 'settings', text: 'Settings'}
     ]
   }
@@ -28,13 +29,16 @@ export class NavComponent implements OnInit {
 
   handleNavigationClicked(navigation: any) {
     switch (navigation.id) {
-
       // handle settings click
       case 6:
         this.dialog.open(SettingsComponent, {
           width: '400px',
           height: '200px'
         })
+        break;
+
+      default:
+        this.router.navigate([navigation.route])
         break;
     }
   }
