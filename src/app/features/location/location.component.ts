ed9f887521c29b7b694ec9f565e83d0d2ac48d1f
Syed Owais Ali <owais.ali@venturedive.com>
2021-10-30 20:05:11 +0500

    location feature completed

--- src/app/features/location/location.component.ts
+++ src/app/features/location/location.component.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationComponent implements OnInit {
 
+  locations = [
+    {
+      city: "Karachi",
+      address: "Plot Number C/21 Landhi C Area, Karachi, Sindh",
+      background: 'assets/location_karachi_bg.jpg',
+      coords: {
+        latitude: 24.843744,
+        longitude: 67.198691,
+      }
+    },
+    {
+      city: "Pakpattan",
+      address: "Abadi Jhughiyan Kundan LaL, Pakpattan, Punjab",
+      background: 'assets/location_pakpattan_bg.jpg',
+      coords: {
+        latitude: 30.344721262825235,
+        longitude: 73.40368348568946,
+      }
+    },
+    {
+      city: "Jhang",
+      address: "78VF+9JX, Chaudry Colony Sadar, Jhang, Punjab",
+      background: 'assets/location_jhang_bg.jpg',
+      coords: {
+        latitude: 31.293498246877967,
+        longitude: 72.3241247958066,
+      }
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  openLocationInGoogleMap(location: any) {
+    window.open(`https://www.google.com/maps/search/?api=1&query=${location.coords.latitude},${location.coords.longitude}`, '_blank')
+  }
 }
