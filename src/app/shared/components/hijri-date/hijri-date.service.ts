a18c6b9d405ebee5776f96102a3a7bf5ecdc3a8e
Syed Owais Ali <owais.ali@venturedive.com>
2021-11-10 12:46:53 +0500

    get hijri-date based on envrionment config

--- src/app/shared/components/hijri-date/hijri-date.service.ts
+++ src/app/shared/components/hijri-date/hijri-date.service.ts
@@ -2,37 +2,42 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable} from "rxjs";
 import { map } from "rxjs/internal/operators/map";
+import {EnvironmentConfigService} from "../../../core/services/environment.config.service";
 
 
 
 @Injectable()
 export class HijriDateService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private envConfig: EnvironmentConfigService) {}
 
   getHijriDate() {
 
-    let url = 'https://api.aladhan.com/v1/gToH?date=' + this.getCurrentDate();
+    let url = 'https://api.aladhan.com/v1/gToH?date=' + HijriDateService.getCurrentDate();
 
-    return new Observable( observer =>
-      observer.next({
-        weekday: this.getWeekDay(),
-        day: "12",
-        month: "صَفَر",
-        year: "1443"
-      })
-    )
-    /*return this.http.get<Observable<any>>(url).pipe(map((response : any) => {
-      return {
-        weekday: this.getWeekDay(),
-        day: response.data.hijri.day,
-        month: response.data.hijri.month.ar,
-        year: response.data.hijri.year
-      }
-    }))*/
+    if (!this.envConfig.production) {
+      return new Observable( observer =>
+        observer.next({
+          weekday: this.getWeekDay(),
+          day: "12",
+          month: "صَفَر",
+          year: "1443"
+        })
+      )
+    }
+    else {
+      return this.http.get<Observable<any>>(url).pipe(map((response : any) => {
+        return {
+          weekday: this.getWeekDay(),
+          day: response.data.hijri.day,
+          month: response.data.hijri.month.ar,
+          year: response.data.hijri.year
+        }
+      }))
+    }
   }
 
-  private getCurrentDate(): String {
+  private static getCurrentDate(): String {
     let date = new Date();
     let d = date.getDate();
     let m = date.getMonth() + 1;
